f0237a6781827c66036f57bf9b23aed038b03c25
erichoonpark <erichoonpark@gmail.com>
2015-04-23 08:37:59 -0700

    Validating Posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416173844) do
+ActiveRecord::Schema.define(version: 20150421174259) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150416173844) do
     t.datetime "updated_at"
     t.string   "name"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
