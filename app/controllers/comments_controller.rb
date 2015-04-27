74df4ae6e1b98a3b3b5bd52e5b27176bee1b372c
erichoonpark <erichoonpark@gmail.com>
2015-04-27 12:45:50 -0700

    Complete destroy assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,22 @@
 class CommentsController < ApplicationController
+  #Destroy Assignment, remember that @topics can no longer be referenced
+  #Must specify topic with @post.topic because no longer nested
+
   def new
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comments = @post.comments
     @comment = @post.comments.build
   end
 
   def create
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comments = @post.comments
     @comment = @post.comments.build(params.require(:comment).permit(:body).merge(user_id: current_user.id))
     @comment.post = @post
 
     if @comment.save
       flash[:notice] = "Comment was created."
-      redirect_to [@topic,@post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
       render 'posts/show'
@@ -23,16 +24,15 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic,@post]
+      redirect_to [@post.topic,@post]
     else
       flash[:error] = "Please try again, comment couldn't be deleted."
-      redirect_to [@topic,@post]
+      redirect_to [@post.topic,@post]
     end
   end
 end
