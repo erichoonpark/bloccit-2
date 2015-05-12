5b7fe9f0435d455efaef726b41a765e257f1ae42
erichoonpark <erichoonpark@gmail.com>
2015-05-12 14:04:38 -0700

    Favorite Assignment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,22 @@ require 'rails_helper'
    include TestFactories
 
    describe "#favorited(post)" do
-     xit "returns `nil` if the user has not favorited the post" do
+
+     before do
+      #Populate the test database with favorites
+      @user = authenticated_user
+      @post = associated_post
+      sign_in @user
      end
 
-     xit "returns the appropriate favorite if it exists" do
+     it "returns `nil` if the user has not favorited the post" do
+       expect( @user.favorites.find_by_post_id(@post.id) ).to be_nil
+       post :create, { post_id: @post.id }
+       expect( @user.favorites.find_by_post_id(@post.id) ).to_not be_nil
+     end
+
+     it "returns the appropriate favorite if it exists" do
+       expect( )
      end
    end
  end
