e1b540bbd62686518af2dcea55490a8c615ccda3
erichoonpark <erichoonpark@gmail.com>
2015-05-22 16:07:48 -0700

    Private Topics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
   redirect_to root_url, alert: exception.message
 end
 
-def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(resource)
      topics_path
-   end
+  end
 
 
      protected
