745460a5dcefde8e3eafc982d3302085c475c55b
erichoonpark <erichoonpark@gmail.com>
2015-05-25 14:35:39 -0700

    Popular Posts Checkpoint

---
+++ app/controllers/topics/posts_controller.rb
@@ -0,0 +1,69 @@
+class Topics::PostsController < ApplicationController
+  def show
+    @topic = Topic.find(params[:topic_id])
+    authorize @topic
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+    @topic = Topic.find(params[:topic_id])
+    authorize @post
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    #@post = current_user.posts.build(post_params)
+    @post = @topic.posts.build(post_params.merge(user_id: current_user.id))
+    authorize @post
+    if @post.save_with_initial_vote(current_user)
+      flash[:notice] = "Post was saved."
+      redirect_to [@topic, @post]
+    else
+      flash[:notice] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post was updated."
+      redirect_to [@post.topic, @post]
+      #[Acts like a directory -> outerdirectory,inner]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an eror deleting the post."
+      render :show
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
